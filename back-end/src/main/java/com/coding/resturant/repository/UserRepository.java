3d09f227eb956fd1ba965c45f5ba8ce6a143a93c
MohamedElsayed2022 <mohamedsayed33333333@gmail.com>
2026-05-12 23:07:14 +0300

    spring secuirty (BeansConfig && SecuirtyConfig && UserDetailsService) #53

---
+++ back-end/src/main/java/com/coding/resturant/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.coding.resturant.repository;
+
+import com.coding.resturant.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+     Optional<User> findByEmail(String email);
+}
