dc2d34f8145775d5b5c3373e0db880c6b2c6f74f
MohamedElsayed2022 <mohamedsayed33333333@gmail.com>
2026-05-13 01:59:41 +0300

    UserPrincipal #55

---
+++ back-end/src/main/java/com/coding/resturant/dto/UserPrincipal.java
@@ -0,0 +1,53 @@
+package com.coding.resturant.dto;
+
+import com.coding.resturant.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Optional;
+
+public class UserPrincipal implements UserDetails {
+    @Autowired
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
