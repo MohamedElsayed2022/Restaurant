c77382cd9bd96b3f20a9e01eca6878343f33c8c2
MohamedElsayed2022 <mohamedsayed33333333@gmail.com>
2026-05-13 01:59:27 +0300

    UserPrincipal #55

--- back-end/src/main/java/com/coding/resturant/springsecurity/secuirty/UserDetailsServiceImpl.java
+++ back-end/src/main/java/com/coding/resturant/springsecurity/secuirty/UserDetailsServiceImpl.java
@@ -1,5 +1,7 @@
 package com.coding.resturant.springsecurity.secuirty;
 
+import com.coding.resturant.dto.UserPrincipal;
+import com.coding.resturant.model.User;
 import com.coding.resturant.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,12 +9,17 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
     private final UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
-     return (UserDetails) userRepository.findByEmail(userEmail).orElseThrow(()->new UsernameNotFoundException("User not found"));
+        User user = userRepository.findByEmail(userEmail);
+        return new UserPrincipal(user);
+        // UserPrincipal userPrincipal = new UserPrincipal(user);
+
     }
 }
